c52fbd063b354d26d978e2b4cf22f7dfe954fcc9
Anurag <anuragsharma921998@gmail.com>
2025-04-28 19:00:48 +0530

    JS-Basics

--- JS-Tasks/script.js
+++ JS-Tasks/script.js
@@ -15,3 +15,28 @@ let math = 100;
 let avg = (hindi + eng + math) /3 ;
 //ANS.90.
 console.log(avg);
+
+//Q.3 Declare your name as a string and print its length in JS ?
+
+let myName = "Anurag";
+console.log(myName);
+let length = myName.length;
+console.log(length);
+
+//Q.4 Declare your first name as a string and print its first character.
+
+let firstName = 'Anurag';
+console.log(firstName[0]);
+
+//Q5. Declare your first name as a string and print its last character.
+
+let lastName = 'Sharma';
+console.log(lastName[5]);
+
+//Q.6 What are lengths of an empty string & a string with a single space ?
+
+let emp = ""; //0
+console.log(emp.length);
+
+let space = " "; //1
+console.log(space.length);

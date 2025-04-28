b045aa2f8ed4c52316700d623b3dc1842ef592ef
Anurag <anuragsharma921998@gmail.com>
2025-04-28 11:01:46 +0530

    JS-Basics

--- JS-Basics-1/script.js
+++ JS-Basics-1/script.js
@@ -1,2 +1,24 @@
 //line below is used to print on the console window
 console.log('Namaste duniya version4');
+
+//Variables
+
+//let Variable!
+let a = 'sharma';
+console.log(a);
+
+//var Variable!
+var lastName = 'anurag';
+console.log(lastName);
+
+//constant Variable!
+const num = 123;
+// num = 321;
+console.log(num);
+
+//Dynamicaly Type
+let age = 'pata nahi';
+console.log(age);
+
+age = 20;
+console.log(age);

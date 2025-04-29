e006acc46af0811ae243ef4bd5072a8808fe90ca
Anurag <anuragsharma921998@gmail.com>
2025-04-29 16:29:01 +0530

    JS-Basics

---
+++ JS-Basics-1/06_else-if.js
@@ -0,0 +1,20 @@
+let age = 4;
+if(age >= 18) {
+    console.log("you can vote")
+}
+else if (age < 18) {
+    console.log("you can't vote")
+}
+
+// marksheet marks:
+let marks = 85;
+
+if(marks >= 80) {
+    console.log("A+");
+}else if(marks >= 60) {
+    console.log("A")
+}else if(marks >+ 33) {
+    console.log("B")
+}else if(marks < 33) {
+    console.log("F")
+}

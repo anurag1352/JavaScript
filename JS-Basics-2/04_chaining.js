83323c4bceda576094465c42029b67aca07ddb56
Anurag <anuragsharma921998@gmail.com>
2025-05-01 16:07:36 +0530

    JS-method-chaining

---
+++ JS-Basics-2/04_chaining.js
@@ -0,0 +1,9 @@
+let msg = "    anurag     ";
+// let newMsg = msg.trim();
+// console.log("after trim :", newMsg);
+// newMsg = newMsg.toUpperCase();
+// console.log("after uppercase :", newMsg);
+
+// shorthand chaining method
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);

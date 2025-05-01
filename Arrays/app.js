53fc24561f8d3455fecdc2b531b78f0f18f031f1
Anurag <anuragsharma921998@gmail.com>
2025-05-01 21:03:12 +0530

    array-creating

---
+++ Arrays/app.js
@@ -0,0 +1,18 @@
+let marks = [99,85,98,78,65]; //number array
+console.log(marks);
+
+let names = ["adam","bob","catlyn"]; //string array
+console.log(names);
+
+let info = ["anurag", 25,9,1.67]; //mixed array
+console.log(info);
+console.log(info.length);
+
+let newArr = []; //empty array
+console.log(newArr)
+
+//Arrays are Mutable.
+let fruits = ["mango","litchi","apple"];
+fruits[0] = "banana kha lo!";
+fruits[10] = "pineapple lalo"
+console.log(fruits);

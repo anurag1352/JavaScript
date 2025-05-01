a227289f07e927bf3078baf6358f0840134b3810
Anurag <anuragsharma921998@gmail.com>
2025-05-01 15:44:42 +0530

    JS-trim-method

---
+++ JS-Basics-2/01_trim-method.js
@@ -0,0 +1,7 @@
+let msg = "     anurag      ";
+let newMsg = console.log(msg.trim());
+console.log(newMsg);
+
+let pswrd = prompt("Set your Password");
+let newPassword = console.log(pswrd.trim());
+console.log(newPassword);

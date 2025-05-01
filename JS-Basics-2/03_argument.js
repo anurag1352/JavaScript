cf969692015daaca36fe8a577df00093706cb226
Anurag <anuragsharma921998@gmail.com>
2025-05-01 16:00:27 +0530

    JS-argument-method

---
+++ JS-Basics-2/03_argument.js
@@ -0,0 +1,9 @@
+let msg = "ILoveCoding";
+let newMsg1 = console.log(msg.indexOf("Love"));
+console.log(newMsg1); //1
+
+let newMsg2 = console.log(msg.indexOf("j"));
+console.log(newMsg2); // -1 (not found)
+
+let newMsg3 = console.log(msg.indexOf("o"));
+console.log(newMsg3); // 2

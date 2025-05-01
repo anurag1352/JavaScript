b7f3266d45b620a2297ee20e7d0d8b8fde688f09
Anurag <anuragsharma921998@gmail.com>
2025-05-01 21:12:44 +0530

    array-methods

---
+++ Arrays/01-array-method.js
@@ -0,0 +1,12 @@
+let cars = ["audi","bmw","kia","xuv"];
+cars.push("toyota","nano"); //push add element at the end.
+cars.pop(); //pop delete element from the end.
+cars.unshift("nano","tuk-tuk") // unshift add element starting of the array.
+cars.shift(); //shift delete element from the ofstarting  array
+console.log(cars);
+
+//eg.
+let follower = ["a","b","c","d"];
+let block = follower.shift();
+console.log(follower);
+console.log(block);

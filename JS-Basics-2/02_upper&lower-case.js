b1ad50c43ad27c516afb0d1420eff373502ffa1c
Anurag <anuragsharma921998@gmail.com>
2025-05-01 15:50:07 +0530

    JS-upper&lower-method

---
+++ JS-Basics-2/02_upper&lower-case.js
@@ -0,0 +1,10 @@
+//UpperCase method in String .
+let str = "Random string";
+let newStr = console.log(str.toUpperCase());
+console.log(newStr);
+
+
+//LowerCase method in String.
+let myName = "ANURAG";
+let newName = console.log(myName.toLowerCase());
+console.log(newName);

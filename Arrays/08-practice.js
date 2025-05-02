d6f3a63b11c7839ee053b0196bce73a4e4b0431d
Anurag <anuragsharma921998@gmail.com>
2025-05-02 15:57:53 +0530

    array-practice

---
+++ Arrays/08-practice.js
@@ -0,0 +1,10 @@
+//Sol.1
+let month = ["january", "july", "march", "august"];
+month.splice(0,1);
+month.splice(1,0, "june");
+console.log(month);
+
+//Sol.2
+let tech = ["sql","c#","java","python","javascript","html","c++","c"];
+tech.reverse();
+console.log(tech);

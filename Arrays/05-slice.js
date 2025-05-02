26d7e86173c17514d551f157db3ecdfe3b7d63d2
Anurag <anuragsharma921998@gmail.com>
2025-05-02 15:21:05 +0530

    array-slice

--- Arrays/05-slice.js
+++ Arrays/05-slice.js
@@ -7,5 +7,8 @@ console.log(slice1);
 let slice2 = colors.slice(2,3);
 console.log(slice2);
 
-let slice3 = colors.slice(-3);
+let slice3 = colors.slice(10);
 console.log(slice3);
+
+let slice4 = colors.slice(-3);
+console.log(slice4);

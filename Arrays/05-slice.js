72a7a8a3cd59406096530181b6c44405f15027ac
Anurag <anuragsharma921998@gmail.com>
2025-05-02 15:17:58 +0530

    array-slice

---
+++ Arrays/05-slice.js
@@ -0,0 +1,11 @@
+let colors = ["red", "yellow", "blue", "green", "pink", "white", "purpule"];
+console.log(colors);
+
+let slice1 = colors.slice(2);
+console.log(slice1);
+
+let slice2 = colors.slice(2,3);
+console.log(slice2);
+
+let slice3 = colors.slice(-3);
+console.log(slice3);

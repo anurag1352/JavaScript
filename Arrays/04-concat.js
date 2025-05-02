7460a84772c184e6abe0db643eb5b25260b59a52
Anurag <anuragsharma921998@gmail.com>
2025-05-02 15:08:25 +0530

    array-concat&reverse

---
+++ Arrays/04-concat.js
@@ -0,0 +1,14 @@
+let primary = ["red","yellow","blue"];
+console.log(primary)
+
+let secondary = ["orange","purpule","violet"];
+console.log(secondary);
+
+//merge 2 arrays
+let final = primary.concat(secondary);
+console.log(final);
+
+
+//reverse an array.
+primary.reverse(); //reverse the array.
+console.log(primary);

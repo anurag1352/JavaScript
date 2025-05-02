a517ff03a254651e59cbc5183b4a77c27bb1cf62
Anurag <anuragsharma921998@gmail.com>
2025-05-02 15:02:26 +0530

    array-indexof&includes

---
+++ Arrays/03-indexOf.js
@@ -0,0 +1,12 @@
+//indexOf.
+let cars = ["audi" ,"bmw", "xuv"];
+console.log((cars.indexOf("xuv")),cars);
+
+let marks = [99,88,77,55,100];
+console.log(marks,(marks.indexOf(100)));
+;
+
+//includes.
+let car = ["audi" ,"bmw", "xuv"];
+console.log((car.includes("xuv")),car); //true
+console.log((car.includes("toyota")),car); //flase.

f62f5bd730d24ec786f38cab358eb23f87c5f438
Anurag <anuragsharma921998@gmail.com>
2025-05-02 15:45:20 +0530

    array-sort

---
+++ Arrays/07-sort.js
@@ -0,0 +1,11 @@
+let num = [19,8,3,6,7,9,5,1,4,2];
+console.log(num)
+
+let sort1 = num.sort();
+console.log(sort1);
+
+let cars = ["bmw","gwagon" ,"toyota" , "maruti","xuv","zen"];
+console.log(cars)
+
+let sort2 = cars.sort();
+console.log(sort2);

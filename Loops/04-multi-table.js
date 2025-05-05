f77bc4c2ed19b9f110af1f4480f226fcc1647069
Anurag <anuragsharma921998@gmail.com>
2025-05-05 10:48:25 +0530

    print-table

---
+++ Loops/04-multi-table.js
@@ -0,0 +1,11 @@
+// for (let table=5; table<=50; table=table+5) {
+//     console.log(table);
+// }
+
+// print table according to user.
+let n = prompt("write your table");
+n = parseInt(n);
+
+for(let i=n; i<=n*10; i=i+n){
+    console.log(i);
+}

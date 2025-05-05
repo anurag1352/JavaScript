4538bb3df9b076574b3e72e7c96b32312177cf22
Anurag <anuragsharma921998@gmail.com>
2025-05-05 10:18:29 +0530

    for-loop

---
+++ Loops/01-for-loop.js
@@ -0,0 +1,15 @@
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+//for-loop.
+for(let i=1; i<=5; i++) {
+    console.log(i);
+}
+
+//backward for loop.
+for(let j=15; j>=1; j = j-3) {
+    console.log(j);
+}

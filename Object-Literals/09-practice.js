075a0ebeed42a63c4767adcb1d5c2d27bc8bf964
Anurag <anuragsharma921998@gmail.com>
2025-05-06 11:33:35 +0530

    practice

---
+++ Object-Literals/09-practice.js
@@ -0,0 +1,11 @@
+//Q.1 Generate a random number between 1 and 100.
+
+//Sol.
+let num = Math.floor(Math.random() * 100) + 1;
+console.log(num);
+
+//Q.2 Generate a random number between 1 and 5.
+
+//Sol.
+let random = Math.floor(Math.random() * 5) + 21;
+console.log(random);

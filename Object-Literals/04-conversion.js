1a6da0866dd7611daeb87afe9deb7a0ff627fe28
Anurag <anuragsharma921998@gmail.com>
2025-05-06 10:21:10 +0530

    conversion

---
+++ Object-Literals/04-conversion.js
@@ -0,0 +1,12 @@
+let obj = {
+    1: "a",
+    2: "b",
+    null: "c",
+    true: "d",
+    undefined: "e"
+};
+console.log(obj.undefined);
+console.log(obj.true);
+console.log(obj.null);
+console.log(obj [2]);
+console.log(obj[1]);

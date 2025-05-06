8034e4381d94a8151641b6a9b796bd46b42aacf1
Anurag <anuragsharma921998@gmail.com>
2025-05-06 10:57:37 +0530

    array-object

---
+++ Object-Literals/07-array-object.js
@@ -0,0 +1,27 @@
+const classInfo = [
+    {
+        name: "anurag",
+        grade: "A",
+        city: "pune"
+    },
+    {
+        name: "angel",
+        grade: "A+",
+        city: "Chandigarh"
+    },
+    {
+        name: "kartik",
+        grade: "O",
+        city: "Gurgaon"
+    }
+];
+
+console.log(classInfo);
+console.log(classInfo[0]);
+
+classInfo[1].city = "pune";
+classInfo[1].gender = "female";
+console.log(classInfo[1]);
+
+console.log(classInfo[2]);
+

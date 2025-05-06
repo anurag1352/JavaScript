e693d6353e3a74916a132e5a7cdf0e1878afbe3d
Anurag <anuragsharma921998@gmail.com>
2025-05-06 11:27:25 +0530

    Random-integer

---
+++ Object-Literals/08-math-object.js
@@ -0,0 +1,10 @@
+//Random Integer.
+
+// let num = Math.random();
+// num = num * 10;
+// num = Math.floor(num);
+// num = num + 1;
+// console.log(num);
+
+let num = Math.floor(Math.random() * 10) + 1;
+console.log(num);

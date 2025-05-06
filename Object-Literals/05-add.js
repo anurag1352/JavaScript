efd2b5b0e50c5fe785c12fec66672fe767ba12e8
Anurag <anuragsharma921998@gmail.com>
2025-05-06 10:27:36 +0530

    add/update-key-value

---
+++ Object-Literals/05-add.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "anurag",
+    age: 20,
+    marks: 89,
+    city: "Noida"
+}
+console.log(student);
+
+student.city = "Pune";
+console.log(student);
+
+student.gender = "Male";
+console.log(student);

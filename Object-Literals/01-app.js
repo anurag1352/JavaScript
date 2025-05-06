66fff0cc56dfb150e54d9772109d137db5c6df07
Anurag <anuragsharma921998@gmail.com>
2025-05-06 09:59:42 +0530

    Create-object-literals

---
+++ Object-Literals/01-app.js
@@ -0,0 +1,21 @@
+//Createing Object-literals.
+const student = {
+    name: "anurag",
+    age: 20,
+    marks: 89,
+    city: "Panchkula"
+};
+console.log(student);
+
+
+//data store in array.
+// let student2 = ["anurag", 20, 89, "panchkula"];
+// console.log(student2);
+
+const item = {
+    price: 100.99,
+    discount: 50,
+    colors : ["red", "black","blue"]
+};
+
+console.log(item);

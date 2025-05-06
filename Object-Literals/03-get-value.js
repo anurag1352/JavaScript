790659a2ee038632767a0c93a9027ade5ef850ee
Anurag <anuragsharma921998@gmail.com>
2025-05-06 10:13:29 +0530

    get-value

---
+++ Object-Literals/03-get-value.js
@@ -0,0 +1,11 @@
+const post = {
+    userName: "@anuragsharma_52",
+    content: "Travelling",
+    likes: 500,
+    repost: 2,
+    tags: ["avara", "traveller"]
+};
+console.log(post);
+console.log(post.tags[1]);
+console.log(post.tags);
+console.log(post ["likes"]);

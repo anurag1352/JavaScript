fbdd30ef7c17078557ef9b06759de09e7153e642
Anurag <anuragsharma921998@gmail.com>
2025-05-06 12:02:02 +0530

    guessing-game

---
+++ Object-Literals/10-Guessing-game.js
@@ -0,0 +1,23 @@
+const max = prompt("enter the max number");
+
+
+const random = Math.floor(Math.random() * max) +1;
+
+let guess = prompt("Guess the number");
+
+while(true) {
+    if(guess == "quit"){
+        console.log("user quit");
+        break;
+    }
+
+    if(guess == random) {
+        console.log("you are right! congrats random number was", random);
+        alert("Congratulations!");
+        break;
+    }else if(guess < random){
+      guess = prompt("hint:your guess was too small, please try again!");
+    }else{
+        guess = prompt("hint:your guess was too large, please try again!");
+    }
+}

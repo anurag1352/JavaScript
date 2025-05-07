3ac275c02943ee976afab0683ab7fb4cc2cbd32f
Anurag <anuragsharma921998@gmail.com>
2025-05-07 14:27:40 +0530

    Create-functions

---
+++ Functions/01-function.js
@@ -0,0 +1,30 @@
+//Function-1
+function helo() {
+    console.log("Hello Bharat!");
+}
+helo();
+
+//Function-2
+function printName() {
+    console.log("Anurag Sharma");
+}
+printName();
+
+//loops in function.
+function print1to5() {
+    for(let i=1; i<=5; i++) {
+        console.log(i);
+    }
+}
+print1to5();
+
+//conditional in functions
+function isAdult() {
+    let age = 18;
+    if(age<=18){
+        console.log("Your adult");
+    }else{
+        console.log("Not Adult");
+    }
+}
+isAdult();

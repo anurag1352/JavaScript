18d86823a3667d4dd21ca70a73fb5fa4ff6cee3a
Anurag <anuragsharma921998@gmail.com>
2025-05-07 14:53:22 +0530

    function-with-arguments

--- Functions/04-fun-argument.js
+++ Functions/04-fun-argument.js
@@ -5,3 +5,11 @@ printInfo("anurag",20,"Delhi");
 printInfo("chota-Bacha",14,"Pune");
 printInfo("karan",20);
 printInfo(23);
+
+function sum(a,b) {
+    console.log(a+b);
+}
+sum(2,4);
+sum(7,5);
+sum(9,2);
+sum(1,3);

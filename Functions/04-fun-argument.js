1326eee2af7b5d77c8d9e2296b54048b776a3923
Anurag <anuragsharma921998@gmail.com>
2025-05-07 14:51:41 +0530

    function-with-argument

---
+++ Functions/04-fun-argument.js
@@ -0,0 +1,7 @@
+function printInfo(name,age,city) {
+    console.log(`${name}'s age is ${age} & he's from ${city}.`);
+}
+printInfo("anurag",20,"Delhi");
+printInfo("chota-Bacha",14,"Pune");
+printInfo("karan",20);
+printInfo(23);

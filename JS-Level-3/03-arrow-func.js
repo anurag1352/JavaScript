0a9faa001c68b8b44f0e78c6e626b8ccb3aa50d2
Anurag <anuragsharma921998@gmail.com>
2025-05-08 11:15:44 +0530

    arrow-functions

---
+++ JS-Level-3/03-arrow-func.js
@@ -0,0 +1,20 @@
+//1-. Arrow functions.
+const sum = (a,b) => {
+    console.log(a+b);
+}
+console.log(sum(67,90));
+
+const cube= (n) => {
+    return n * n * n;
+}
+console.log(cube(3));
+
+const power = (a,b) => {
+    return a ** b;
+}
+console.log(power(9,2));
+
+const helo = () =>{
+    console.log("hello");
+}
+console.log(helo());

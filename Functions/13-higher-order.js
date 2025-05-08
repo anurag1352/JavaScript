f221bcfc8f053b5ce12846dbc653816c5767d9b4
Anurag <anuragsharma921998@gmail.com>
2025-05-08 09:33:31 +0530

    Higher-order-func

--- Functions/13-higher-order.js
+++ Functions/13-higher-order.js
@@ -10,3 +10,36 @@ let greet = function() {
 multipleGreet(greet,2);
 
 multipleGreet(function() {console.log("namaste")}, 90); //same order in single line.
+
+// let odd = function (n) {
+//     console.log(!(n%2 == 0));
+// }
+// console.log(odd(4));
+
+// let even = function (n) {
+//     console.log((n%2 == 0));
+// }
+// console.log(even(4));
+
+function oddOrEvenFactory(request) {
+    if(request == "odd") {
+        let odd = function (n) {
+            console.log(!(n%2 == 0));
+        }
+
+        return odd;
+    }else if(request == "even"){
+        let even = function (n) {
+            console.log((n%2 == 0));
+        }
+
+        return even;
+    }else{
+        console.log("Wrong rquest");
+    }
+}
+
+let request = "odd"; //even
+let func = oddOrEvenFactory(request);
+console.log(func(8));
+console.log(func(21));

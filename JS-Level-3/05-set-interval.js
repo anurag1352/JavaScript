7c5e8760f992b624c143c5c8a5f9d39d59a6941c
Anurag <anuragsharma921998@gmail.com>
2025-05-08 11:35:34 +0530

    set-interval

---
+++ JS-Level-3/05-set-interval.js
@@ -0,0 +1,11 @@
+let id =setInterval( () => {
+    console.log("Apna college");
+},2000);
+console.log(id);
+clearInterval(id);
+
+let id2 =setInterval( () => {
+    console.log("Hello bharat");
+},3000);
+console.log(id2);
+clearInterval(id2);

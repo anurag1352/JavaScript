df49b49a7b4cbafcc87e1c20bbcd2fc98facb7d5
Anurag <anuragsharma921998@gmail.com>
2025-05-09 17:03:25 +0530

    spread(Array-literals)

--- JS-Level-4/08-spread.js
+++ JS-Level-4/08-spread.js
@@ -2,3 +2,20 @@ let arr = [1,2,3,4,5,-9,10,1,0];
 console.log(Math.min(...arr));
 console.log(Math.max(...arr));
 console.log(...arr);
+
+//Spread with array literals.
+let str = ["hello bharat"];
+let newStr = [...str];
+newStr.push("jai hind");
+
+console.log(str);
+console.log(newStr);
+
+let char = [..."hello"];
+console.log(char);
+
+let odd = [1,3,5,7,9];
+let even = [2,4,6,8,10];
+
+let nums = [...odd, ...even];
+console.log(nums);
